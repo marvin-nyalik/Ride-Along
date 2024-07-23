a4e3a944b3aab810225ce9c5cd8b3d82b0402c0f
MARVIN <o.silverygold@gmail.com>
2024-07-23 19:02:19 +0300

    Define settings route and pages

---
+++ app/settings/bottles.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class bottles extends Component {
+  render() {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <Text> Bottle textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+export default bottles
---
+++ app/settings/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class index extends Component {
+  render() {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <Text> Settings textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+export default index
