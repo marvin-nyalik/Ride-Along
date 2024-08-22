c6383d8b74e6c52f8b7a2c4f0c7516524ea80584
MARVIN <o.silverygold@gmail.com>
2024-08-22 22:30:05 +0300

    Update onboarding with Swiper

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,12 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, TouchableOpacity, View, Image } from "react-native";
+import React, { useRef, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { onboarding } from "@/assets/constants";
+import CustomButton from "@/components/CustomButton";
 
-const welcome = () => {
+const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
-    <View>
-      <Text>welcome</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex h-full justify-between items-center bg-white">
+      <TouchableOpacity
+        className="flex w-full justify-end items-end p-5"
+        onPress={() => {
+          router.replace("/(auth)/signup");
+        }}
+      >
+        <Text className="text-black text-md font-JakartaBold">skip</Text>
+      </TouchableOpacity>
 
-export default welcome
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View className="flex items-center justify-center p-5">
+            <Image
+              source={item.image}
+              resizeMode="contain"
+              className="w-full h-[300px]"
+            />
+            <View className="flex w-full flex-row items-center justify-center mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
+      <CustomButton title="Next" className={'w'}/>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
