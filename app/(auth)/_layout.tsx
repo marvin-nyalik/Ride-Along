8ff0b719cf1462a69509a7f3a9abeec620ca2fe4
MARVIN <o.silverygold@gmail.com>
2024-08-22 12:46:45 +0300

    initialize onboarding route groups

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="signup" options={{ headerShown: false }} />
+      <Stack.Screen name="signin" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,19 +1,18 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
+      <SafeAreaView style={styles.container}>
+        <Text>This screen doesn't exist.</Text>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <Text>Go to home screen!</Text>
         </Link>
-      </ThemedView>
+      </SafeAreaView>
     </>
   );
 }
