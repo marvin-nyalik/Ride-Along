dfee3f3910fc6a2663059171ccfc9a6fe4ab01fb
MARVIN <o.silverygold@gmail.com>
2024-08-23 12:31:55 +0300

    Define reusable button component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,5 +1,36 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { ButtonProps } from "@/types/type";
+
+const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
+  switch (variant) {
+    case "secondary":
+      return "bg-gray-500";
+    case "danger":
+      return "bg-red-500";
+    case "success":
+      return "bg-green-500";
+    case "outline":
+      return "bg-transparent border-neutral-300 border-[0.5px]";
+    default:
+      return "bg-[#0286ff]";
+  }
+};
+
+const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
+  switch (variant) {
+    case "secondary":
+      return "text-gray-100";
+    case "danger":
+      return "text-red-100";
+    case "success":
+      return "text-green-100";
+    case "primary":
+      return "text-black";
+    default:
+      return "text-white";
+  }
+};
 
 const CustomButton = ({
   onPress,
@@ -10,14 +41,17 @@ const CustomButton = ({
   IconRight,
   className,
   ...props
-}) => (
+}: ButtonProps) => (
   <TouchableOpacity
-  onPress={onPress}
-  className={`w-full rounded-full flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}}`
+    onPress={onPress}
+    className={`w-full rounded-full p-3 flex flex-row shadow-md justify-center items-center shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+    {...props}
   >
-    {IconLeft && <IconLeft/>}
-    <Text>{title}</Text>
-    {IconRight && <IconRight/>}
+    {IconLeft && <IconLeft />}
+    <Text className={`${getTextVariantStyle(textVariant)} text-lg font-JakartaBold`}>
+      {title}
+    </Text>
+    {IconRight && <IconRight />}
   </TouchableOpacity>
 );
 
