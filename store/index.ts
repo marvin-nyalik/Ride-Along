cc27d1fa3da0376b78f9077a8381707ce0aef7b8
MARVIN <o.silverygold@gmail.com>
2024-08-29 12:42:08 +0300

    add DriverStore to global state

--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,4 @@
-import { LocationStore } from "@/types/type";
+import { DriverStore, LocationStore, MarkerData } from "@/types/type";
 import { create } from "zustand";
 
 export const useLocationStore = create<LocationStore>((set) => ({
@@ -17,11 +17,11 @@ export const useLocationStore = create<LocationStore>((set) => ({
     longitude: number;
     address: string;
   }) => {
-    set(()=>({
-        userLatitude: latitude,
-        userLongitude: longitude,
-        userAddress: address,
-    }))
+    set(() => ({
+      userLatitude: latitude,
+      userLongitude: longitude,
+      userAddress: address,
+    }));
   },
   setDestinationLocation: ({
     latitude,
@@ -32,10 +32,18 @@ export const useLocationStore = create<LocationStore>((set) => ({
     longitude: number;
     address: string;
   }) => {
-    set(()=>({
-        destinationLatitude: latitude,
-        destinationLongitude: longitude,
-        destinationAddress: address,
-    }))
+    set(() => ({
+      destinationLatitude: latitude,
+      destinationLongitude: longitude,
+      destinationAddress: address,
+    }));
   },
+}));
+
+export const useDriverStore = create<DriverStore>((set) => ({
+  drivers: [] as MarkerData[],
+  selectedDriver: null,
+  setSelectedDriver: (driverId) => set(() => ({ selectedDriver: driverId })),
+  setDrivers: (drivers: MarkerData[]) => set(() => ({ drivers: drivers })),
+  clearSelectedDriver: () => set(() => ({ selectedDriver: null })),
 }));
